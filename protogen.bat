ea071e2e92aaf5eac92eed126a6721a730ff7180
Regan Laitila <github@reganlaitila.net>
2017-02-22 22:45:24 -0500

    Dev

--- protogen.bat
+++ protogen.bat
@@ -2,13 +2,13 @@
 
 set cwd=%cd%
 
-set PATH=%GOPATH%\bin;%GOPATH%\src\github.com/deviceio\tools\protoc\windows\bin;%PATH%
+set PATH=%GOPATH%\bin;%GOPATH%\src\github.com\deviceio\tools\protoc\windows\bin;%PATH%
 
-set PROTOCBIN="%GOPATH%\src\github.com/deviceio\tools\protoc\windows\bin\protoc.exe"
-set PROTODIR="%GOPATH%\src\github.com/deviceio\shared\protocol_v1"
+set PROTOCBIN="%GOPATH%\src\github.com\deviceio\tools\protoc\windows\bin\protoc.exe"
+set PROTODIR=%GOPATH%\src\github.com\deviceio\shared\protocol\
 
 cd %PROTODIR%
 
-%PROTOCBIN% --go_out=plugins=grpc:%PROTODIR% *.proto
+%PROTOCBIN% --go_out=plugins=grpc:%PROTODIR% protocol.proto
 
 cd %cwd%
